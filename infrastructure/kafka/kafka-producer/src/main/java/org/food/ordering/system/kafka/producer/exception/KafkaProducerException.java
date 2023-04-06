06abb6184e4365e70d0e7c97d903e6e5a04ef00c
mecemis <ecemis.murat@gmail.com>
2023-04-07 00:32:52 +0300

    dataaccess repository added

---
+++ infrastructure/kafka/kafka-producer/src/main/java/org/food/ordering/system/kafka/producer/exception/KafkaProducerException.java
@@ -0,0 +1,8 @@
+package org.food.ordering.system.kafka.producer.exception;
+
+public class KafkaProducerException extends RuntimeException {
+
+    public KafkaProducerException(String message) {
+        super(message);
+    }
+}
--- order-service/order-domain/order-application-service/src/main/java/org/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
+++ order-service/order-domain/order-application-service/src/main/java/org/food/ordering/system/order/service/domain/OrderTrackCommandHandler.java
@@ -3,6 +3,11 @@ package org.food.ordering.system.order.service.domain;
 import lombok.extern.slf4j.Slf4j;
 import org.food.ordering.system.order.service.domain.dto.track.TrackOrderQuery;
 import org.food.ordering.system.order.service.domain.dto.track.TrackOrderResponse;
+import org.food.ordering.system.order.service.domain.entity.Order;
+import org.food.ordering.system.order.service.domain.exception.OrderNotFoundException;
+import org.food.ordering.system.order.service.domain.mapper.OrderDataMapper;
+import org.food.ordering.system.order.service.domain.ports.output.repository.OrderRepository;
+import org.food.ordering.system.order.service.domain.valueobject.TrackingId;
 import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,7 +16,23 @@ import java.util.Optional;
 @Slf4j
 @Component
 public class OrderTrackCommandHandler {
+    private final OrderDataMapper orderDataMapper;
+
+    private final OrderRepository orderRepository;
+
+    public OrderTrackCommandHandler(OrderDataMapper orderDataMapper, OrderRepository orderRepository) {
+        this.orderDataMapper = orderDataMapper;
+        this.orderRepository = orderRepository;
+    }
+    @Transactional(readOnly = true)
     public TrackOrderResponse trackOrder(TrackOrderQuery trackOrderQuery){
-        return null;
+        Optional<Order> orderResult =
+                orderRepository.findByTrackingId(new TrackingId(trackOrderQuery.getOrderTrackingId()));
+        if (orderResult.isEmpty()) {
+            log.warn("Could not find order with tracking id: {}", trackOrderQuery.getOrderTrackingId());
+            throw new OrderNotFoundException("Could not find order with tracking id: " +
+                    trackOrderQuery.getOrderTrackingId());
+        }
+        return orderDataMapper.orderToTrackOrderResponse(orderResult.get());
     }
 }
